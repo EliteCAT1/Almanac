1f480740b686c20f3e4b5f3a822bb02675594531
EliteCAT <Elitecat1@gmail.com>
2021-01-11 18:58:37 -0800

    Unity Setup
    Adding Initial Files

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 7f;
+    public float stoppingRange = 1.5f;
+
+
+    void Update()
+    {
+        Vector3 displacementFromTarget = target.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed;
+
+        float distanceToTarget = displacementFromTarget.magnitude;
+
+        if (distanceToTarget > stoppingRange)
+        {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+}
